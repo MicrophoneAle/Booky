279e6e55c5acfd8b10147d052dfa00a655fbbd92
Michael Liu <michaelliu2016@gmail.com>
2026-05-26 13:59:09 +0800

    attempt to fix upload

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ const app = express()
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://booky-lemon.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://booky-lemon.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 )
 
