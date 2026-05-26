7764941a80cd27ad547ebc56c3e9013331a630a9
Michael Liu <michaelliu2016@gmail.com>
2026-05-26 12:22:01 +0800

    decreased header size

--- client/src/components/BookViewer.jsx
+++ client/src/components/BookViewer.jsx
@@ -59,8 +59,8 @@ function formatPageCounter(leftPage, rightPage, totalPages) {
 function FullscreenIcon() {
   return (
     <svg
-      width="16"
-      height="16"
+      width="12"
+      height="12"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
