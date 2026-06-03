39b621d926f2ff6d7972175b1550752f4738b945
Michael Liu <michaelliu2016@gmail.com>
2026-06-03 12:34:01 +0800

    reverted changes

--- client/src/components/BookViewer.jsx
+++ client/src/components/BookViewer.jsx
@@ -20,7 +20,7 @@ const PAGE_HEIGHT_PX = 600
 const MOBILE_FULLSCREEN_PAGE_HEIGHT_PX = 780
 const SPINE_PX = 1
 const PAGE_NUMBER_RESERVED_PX = 20
-const MOBILE_PAGE_NUMBER_RESERVED_PX = 52
+const MOBILE_FULLSCREEN_PAGE_NUMBER_RESERVED_PX = 44
 const CONTENT_HEIGHT_SAFETY_BUFFER_PX = 8
 const TRIVIAL_LAST_PAGE_CHAR_LIMIT = 50
 const GREEDY_PAGE_SAFETY_MARGIN_PX = 10
@@ -1954,8 +1954,8 @@ export default function BookViewer({
       const flatBlocks = flattenDocument(bookDocument)
       const mobileFS = isMobile && isMobileFullscreen
       const pageHeightToUse = mobileFS ? MOBILE_FULLSCREEN_PAGE_HEIGHT_PX : undefined
-      const pageNumberReservedPx = isMobile
-        ? MOBILE_PAGE_NUMBER_RESERVED_PX
+      const pageNumberReservedPx = mobileFS
+        ? MOBILE_FULLSCREEN_PAGE_NUMBER_RESERVED_PX
         : PAGE_NUMBER_RESERVED_PX
       const { pageOuterHeight, contentMaxHeight } = getLayoutHeights(
         pageHeightToUse,
@@ -1972,9 +1972,7 @@ export default function BookViewer({
       measureElements.page.style.paddingLeft = pagePad.paddingLeft ?? "0"
       measureElements.page.style.paddingBottom = "0"
       measureElements.body.style.padding = "0"
-      measureElements.body.style.paddingBottom = isMobile
-        ? "4px"
-        : `${PAGE_NUMBER_RESERVED_PX}px`
+      measureElements.body.style.paddingBottom = `${PAGE_NUMBER_RESERVED_PX}px`
       measureElements.footer.style.height = `${pageNumberReservedPx}px`
 
       const font = FONT_SIZE_MAP[settings.fontSize] ?? FONT_SIZE_MAP.medium
